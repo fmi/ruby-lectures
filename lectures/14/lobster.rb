6a295f287775fe4052ef672728b3b6e7f78e5ac7
Dimitar Dimitrov <me@ddimitrov.name>
2012-12-05 15:06:42 +0200

    WIP on lecture 14 - Sinatra

--- lectures/14/lobster.rb
+++ lectures/14/lobster.rb
@@ -11,24 +11,6 @@ module Rack
     t1drVmiR42HcWNz5w3QanT+2gIvTVCiE1lm1Y0eU4JGmIIbaKwextKn8rvW+p5PIwFl8ZWJ
     I8jyiTlhTcYXkekJAzTyYN6E08A+dk8voBkAVTJQ==".delete("\n ").unpack("m*")[0])
 
-    LambdaLobster = lambda { |env|
-      if env["QUERY_STRING"].include?("flip")
-        lobster = LobsterString.split("\n").
-          map { |line| line.ljust(42).reverse }.
-          join("\n")
-        href = "?"
-      else
-        lobster = LobsterString
-        href = "?flip"
-      end
-
-      content = ["<title>Lobstericious!</title>",
-                 "<pre>", lobster, "</pre>",
-                 "<a href='#{href}'>flip!</a>"]
-      length = content.inject(0) { |a,e| a+e.size }.to_s
-      [200, {"Content-Type" => "text/html", "Content-Length" => length}, content]
-    }
-
     def call(env)
       req = Request.new(env)
       if req.GET["flip"] == "left"
@@ -52,14 +34,5 @@ module Rack
       res.write "<p><a href='?flip=crash'>crash!</a></p>"
       res.finish
     end
-
   end
-end
-
-if $0 == __FILE__
-  require 'rack'
-  require 'rack/showexceptions'
-  Rack::Handler::WEBrick.run \
-    Rack::ShowExceptions.new(Rack::Lint.new(Rack::Lobster.new)),
-    :Port => 9292
 end
