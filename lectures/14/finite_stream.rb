8fa5aa6c333885ef0ebc3f1bdc97a2983a01778f
Dimitar Dimitrov <me@ddimitrov.name>
2012-12-05 22:16:08 +0200

    Updates to lecture 14

--- lectures/14/finite_stream.rb
+++ lectures/14/finite_stream.rb
@@ -3,9 +3,9 @@ require 'sinatra'
 get '/stream' do
   stream do |connection|
     connection << "Start of the stream\n"
-    sleep 1
+    sleep 3
     connection << "Some more data\n"
-    sleep 1
+    sleep 3
     connection << "And even more data\n"
   end
 end
