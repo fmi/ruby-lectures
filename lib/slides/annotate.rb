f0d0ed675963d941eda2160a61a7560ee1934257
Dimitar Dimitrov <me@ddimitrov.name>
2014-01-05 22:23:34 +0200

    Make annotate work with UTF-8
    If we have non-ASCII chars (ie. unicode symbols) in the source code,
    the annotate macro would fail with an exception deep inside XMPFilter.
    
    XMPFilter does something stupid (in my opinion), by forcing the default
    external encoding to ASCII-8BIT.

--- lib/slides/annotate.rb
+++ lib/slides/annotate.rb
@@ -10,12 +10,18 @@ class Annotate < Slim::Filter
   private
 
   def annotate(input)
-    input = input.gsub /^(.*) #!$/, '(\1) rescue $!.class # =>'
+    input = input.gsub /^(.*) #!$/u, '(\1) rescue $!.class # =>'
+
+    # Make sure the XMPFilter class is loaded, because it sets the default
+    # encoding for external strings to ASCII-8BIT and we need to revert it to
+    # something better, like UTF-8.
+    Rcodetools::XMPFilter
+    Encoding.default_external = 'UTF-8'
 
     output = Rcodetools::XMPFilter.new(warnings: false).annotate(input).join('')
 
-    output = output.gsub /^\((.*)\) rescue \$!\.class # =>\s*(.*)$/, '\1 # error: \2'
-    output = output.gsub /# +=> (.*)/, '# \1'
+    output = output.gsub /^\((.*)\) rescue \$!\.class # =>\s*(.*)$/u, '\1 # error: \2'
+    output = output.gsub /# +=> (.*)/u, '# \1'
     output
   end
 end
