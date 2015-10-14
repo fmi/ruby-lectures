919e6d23a838ed3db716e62f100e0177dbfd38ca
Dimitar Dimitrov <me@ddimitrov.name>
2015-10-14 12:55:09 +0300

    Make our custom error annotator #! work on the next line too
    As with the # => annotator, our error annotator #! can now be put on the
    next line, like so:
    
    	1 / 0
    	#!
    
    It will work as expected.

--- lib/slides/annotate.rb
+++ lib/slides/annotate.rb
@@ -10,7 +10,8 @@ class Annotate < Slim::Filter
   private
 
   def annotate(input)
-    input = input.gsub /^(.*) #!$/u, '(\1) rescue $!.class # =>'
+    input = input.gsub /^(.*)\n(\s*)#!$/u, '(\1) rescue $!.class \2 # =>'
+    input = input.gsub /^(.*) ?#!$/u, '(\1) rescue $!.class # =>'
 
     # Make sure the XMPFilter class is loaded, because it sets the default
     # encoding for external strings to ASCII-8BIT and we need to revert it to
@@ -20,6 +21,7 @@ class Annotate < Slim::Filter
 
     output = Rcodetools::XMPFilter.new(warnings: false, width: 0).annotate(input).join('')
 
+    output = output.gsub /^\((.*)\) rescue \$!\.class (\s*) # =>\s*(.*)$/u, "\\1\n\\2# => error: \\3"
     output = output.gsub /^\((.*)\) rescue \$!\.class # =>\s*(.*)$/u, '\1 # => error: \2'
     output
   end
