732ab6a31fbffd63e0c48e7211986e2ba5c7aa02
Dimitar Dimitrov <me@ddimitrov.name>
2015-12-14 23:37:07 +0200

    Preserve the code in `backticks` as verbatim
    Don't do any replacements of the contents between two backticks and
    also escape special HTML chars so that the text between the backticks
    is preserved and displayed as-is. For example:
    
        list:
          `this --text [is](preserved) <verbatim>`

--- lib/slides/list.rb
+++ lib/slides/list.rb
@@ -1,12 +1,61 @@
 class List < Slim::Filter
+  class Line
+    def initialize(line)
+      @line = line
+    end
+
+    def render
+      extract_verbatim_code_blocks
+      convert_double_dash_to_mdash
+      parse_github_links
+      parse_markdown_links
+      inject_verbatim_code_blocks
+
+      @line
+    end
+
+    private
+
+    def convert_double_dash_to_mdash
+      @line = @line.gsub('--', '&mdash;')
+    end
+
+    def parse_github_links
+      @line = @line.gsub(/\[github:([^\]]*?)\]/, '<a href="http://github.com/\1">\1</a>')
+    end
+
+    def parse_markdown_links
+      @line = @line.gsub(/\[([^\]]+)\]\((\S+?)\)/, '<a href="\2">\1</a>')
+    end
+
+    def extract_verbatim_code_blocks
+      @verbatim_code_blocks = {}
+
+      @line = @line.gsub(/`(.*?)`/) do |match|
+        code = match[1...-1]
+        slug = "<CODE_BLOCK_#{@verbatim_code_blocks.size}_PLACEHOLDER>"
+
+        @verbatim_code_blocks[slug] = "<code>#{h code}</code>"
+
+        slug
+      end
+    end
+
+    def inject_verbatim_code_blocks
+      @verbatim_code_blocks.each do |slug, code|
+        @line = @line.gsub(slug, code)
+      end
+    end
+
+    def h(html)
+      Temple::Utils.escape_html html
+    end
+  end
+
   def on_slim_embedded(engine, body)
     code = Slim::CollectText.new.call(body)
     items = code.lines.map do |line|
-      line = line.gsub(/`(.*?)`/, '<code>\1</code>')
-      line = line.gsub('--', '&mdash;')
-      line = line.gsub(/\[github:([^\]]*?)\]/, '<a href="http://github.com/\1">\1</a>')
-      line = line.gsub(/\[([^\]]+)\]\((\S+?)\)/, '<a href="\2">\1</a>')
-      %{<li class="action">#{line}</li>}
+      %{<li class="action">#{Line.new(line).render}</li>}
     end
     html = "<ul>#{items.join}</ul>"
     [:static, html]
